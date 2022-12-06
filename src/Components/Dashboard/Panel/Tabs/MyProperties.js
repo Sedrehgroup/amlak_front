ae796bc67b7c03c51dd96e73ccc16003d67bf9fa
frontend-mahdi <mahdi.313.ab@gmail.com>
2022-12-07 00:00:12 +0330

    property filter works

--- src/Components/Dashboard/Panel/Tabs/MyProperties.js
+++ src/Components/Dashboard/Panel/Tabs/MyProperties.js
@@ -29,6 +29,41 @@ const MyProperties = () => {
 
   const dispatch = useDispatch();
   const [token] = useToken();
+  useEffect(() => {
+    if (province == "") setCity("");
+  }, [province]);
+  useEffect(() => {
+    const Api_Url = process.env.REACT_APP_API_URL;
+    if (token.length > 0) {
+      setShowLoading(true);
+      try {
+        axios
+          .get(
+            `${Api_Url}/api/property/my_properties/?city=${city}&county=${province}`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          )
+          .then(({ data }) => {
+            setMyProperties(data?.results);
+            setShowLoading(false);
+            setNextUrl(data?.next?.slice(37) || "disable");
+            console.log(
+              "axios get /api/property/my_properties data.data:",
+              data
+            );
+          })
+          .catch((e) => {
+            console.log("error in axios /api/property/my_properties", e);
+            setShowLoading(false);
+          });
+      } catch (error) {
+        console.log("error", error);
+      }
+    }
+  }, [city]);
   const nextHandler = () => {
     const Api_Url = process.env.REACT_APP_API_URL;
     try {
@@ -143,60 +178,60 @@ const MyProperties = () => {
     <>
       <Switch>
         <Route exact path={path}>
-          {!showLoading ? (
-            <div>
-              {MyPropertiesList.length > 0 ? (
-                <>
-                  <div className="flex flex-col gap-4">
-                    <div className="flex flex-row items-baseline p-2 pr-6">
-                      <div className="relative inputC mx-1 mt-6 border-12 border-solid border-white">
-                        <label className="absolute bottom-14 right-0 text-sm text-warmGray-500">
-                          استان
-                        </label>
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-row items-baseline p-2 pr-6">
+              <div className="relative inputC mx-1 mt-6 border-12 border-solid border-white">
+                <label className="absolute bottom-14 right-0 text-sm text-warmGray-500">
+                  استان
+                </label>
 
-                        <select
-                          dir="ltr"
-                          className="w-full h-12 px-4 "
-                          placeholder="تهران"
-                          value={province}
-                          onChange={(e) => setProvince(e.target.value)}
-                        >
-                          <option value={""}>...انتخاب کنید</option>
+                <select
+                  dir="ltr"
+                  className="w-full h-12 px-4 "
+                  placeholder="تهران"
+                  value={province}
+                  onChange={(e) => setProvince(e.target.value)}
+                >
+                  <option value={""}>همه استان ها</option>
 
-                          {[
-                            ...new Set(
-                              iranCitiesList.map((element) => element.province)
-                            ),
-                          ].map((val, index) => (
-                            <option key={index} value={val}>
-                              {val}
-                            </option>
-                          ))}
-                        </select>
-                      </div>
-                      <div className="relative inputC mx-1 mt-6 border-12 border-solid border-white">
-                        <label className="absolute  bottom-14 right-0 text-sm text-warmGray-500">
-                          شهرستان
-                        </label>
-                        <select
-                          dir="ltr"
-                          className="w-full h-12 px-4"
-                          placeholder="تهران"
-                          value={city}
-                          onChange={(e) => setCity(e.target.value)}
-                        >
-                          <option value={""}>...انتخاب کنید</option>
-                          {iranCitiesList
-                            .filter((element) => element.province == province)
-                            .map((val, index) => (
-                              <option key={index} value={val.city}>
-                                {val.city}
-                              </option>
-                            ))}
-                        </select>
-                      </div>
-                    </div>
-                    <div className="mx-8 bg-warmGray-100 h-[1px]"></div>
+                  {[
+                    ...new Set(
+                      iranCitiesList.map((element) => element.province)
+                    ),
+                  ].map((val, index) => (
+                    <option key={index} value={val}>
+                      {val}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="relative inputC mx-1 mt-6 border-12 border-solid border-white">
+                <label className="absolute  bottom-14 right-0 text-sm text-warmGray-500">
+                  شهرستان
+                </label>
+                <select
+                  dir="ltr"
+                  className="w-full h-12 px-4"
+                  placeholder="تهران"
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                >
+                  <option value={""}>همه شهرستان ها</option>
+                  {iranCitiesList
+                    .filter((element) => element.province == province)
+                    .map((val, index) => (
+                      <option key={index} value={val.city}>
+                        {val.city}
+                      </option>
+                    ))}
+                </select>
+              </div>
+            </div>
+            <div className="mx-8 bg-warmGray-100 h-[1px]"></div>
+            {!showLoading ? (
+              <div>
+                {MyPropertiesList.length > 0 ? (
+                  <>
                     <div className="m_grid-container pr-6">
                       {MyPropertiesList.map((val, index) => (
                         <div key={index}>
@@ -228,20 +263,21 @@ const MyProperties = () => {
                         </button>
                       </div>
                     )}
+                  </>
+                ) : null}
+
+                {MyPropertiesList.length == 0 && (
+                  <div className="m-auto text-center text-xl bg-warmGray-300">
+                    آگهی ثبت شده ای وجود ندارد!
                   </div>
-                </>
-              ) : null}
-              {MyPropertiesList.length == 0 && (
-                <div className="m-auto text-xl bg-warmGray-300">
-                  آگهی ثبت شده ای وجود ندارد!
-                </div>
-              )}
-            </div>
-          ) : (
-            <div className="flex justify-center items-center eightyvh">
-              <Spinner name="folding-cube" color="#FF731D" fadeIn="none" />
-            </div>
-          )}
+                )}
+              </div>
+            ) : (
+              <div className="flex justify-center items-center eightyvh">
+                <Spinner name="folding-cube" color="#FF731D" fadeIn="none" />
+              </div>
+            )}
+          </div>
         </Route>
         <Route path={`${path}/:cardId`}>
           <PropertyDetails data={adData} />
