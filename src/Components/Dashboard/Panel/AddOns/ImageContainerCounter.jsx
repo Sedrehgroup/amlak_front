917a41feffea69b29ccb2e3d94c353d21a3d8940
Mohamad Ganji <mohamad.ganjif@gmail.com>
2022-12-07 13:38:11 +0330

    css changes is done

--- src/Components/Dashboard/Panel/AddOns/ImageContainerCounter.jsx
+++ src/Components/Dashboard/Panel/AddOns/ImageContainerCounter.jsx
@@ -7,21 +7,21 @@ export default function ImageContainerCounter() {
     <div className="eightyfivepercent flex gap-4 mb-4 ">
       <div className="w-1/3 relative">
         <img src={waiting} alt="waiting" className="h-48 " />
-        <div className="bg-main-100 absolute p-1 top-0 right-1 rounded-tr-lg rounded-bl-lg">
+        <div className="bg-main-100 absolute p-1 top-2 right-0 rounded-tr-lg rounded-bl-lg">
           منتظر بررسی
         </div>
         <span className="text-white absolute top-1/2 left-1/2 text-5xl">5</span>
       </div>
       <div className="w-1/3 relative">
         <img src={rents} alt="rents" className="h-48" />
-        <div className="bg-main-100 absolute p-1 flex justify-center items-center top-0 right-0 rounded-tr-lg rounded-bl-lg">
+        <div className="bg-main-100 absolute p-1 flex justify-center items-center top-2 right-0 rounded-tr-lg rounded-bl-lg">
           اجاره شده ها{" "}
         </div>
         <span className="text-white absolute top-1/2 left-1/2 text-5xl">5</span>
       </div>
       <div className="w-1/3 relative">
         <img src={rented_counter} alt="rented_counter" className="h-48" />
-        <div className="bg-main-100 absolute p-1 flex justify-center items-center top-0 right-0 rounded-tr-lg rounded-bl-lg">
+        <div className="bg-main-100 absolute p-1 flex justify-center items-center top-2 right-0 rounded-tr-lg rounded-bl-lg">
           اجاره داده شده ها{" "}
         </div>
         <span className="text-white absolute top-1/2  text-5xl">5</span>
