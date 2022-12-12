9333f992e085c297f9f5564b7e538fb21d643acd
frontend-mahdi <mahdi.313.ab@gmail.com>
2022-12-12 14:45:54 +0330

    registration done with new api

--- src/Components/Dashboard/BackgroundAndPanel.js
+++ src/Components/Dashboard/BackgroundAndPanel.js
@@ -16,7 +16,7 @@ export default function BackgroundAndPanel() {
           maxHeight: "calc(100vh - 72px)",
           overflowY: "auto",
         }}
-        className=""
+      
       >
         <Dashboard />
       </div>
