04234d4ebc8427f4ff712a2da712a94e9dd6cdd7
mohammadT9030 <torabi9030@gmail.com>
2022-12-14 14:44:06 +0330

    add popup map in submit property

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,4 +3,5 @@ export const LEAFLET_TILELAYER_URL = "https://{s}.tile.openstreetmap.org/{z}/{x}
 export const LEAFLET_CENTER = [35.6996786,51.3381075];
 export const LEAFLET_ZOOM = 13;
 export const LEAFLET_SCROLLZOOM = false;
+export const DOES_MAP_FLY_ONCLICK = true;
 
