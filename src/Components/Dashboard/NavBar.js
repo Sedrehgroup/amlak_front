477bbf1b4ace15847e13e374c3151b1b57846c8c
Mohamad Ganji <mohamad.ganjif@gmail.com>
2022-12-15 10:24:08 +0330

    fix redirect to login

--- src/Components/Dashboard/NavBar.js
+++ src/Components/Dashboard/NavBar.js
@@ -54,8 +54,12 @@ export default function NavBar() {
            
           });
       } catch (error) {
+
         console.log("error", error);
       }
+    }
+    else {
+      dispatch(setUserIsLoggedHandler(false))
     }
   }, [token,update]);
 
